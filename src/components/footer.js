d2d66710586a298a55d4dd3c5bcbf2c0d98e32a1
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-05-18 22:33:08 +0900

    create footer contents

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
-// import PropTypes from "prop-types"
 
-const Footer = () => {
+const Footer = () => (
   <footer>
-    <div>
-      
+    <div className="topFooter">
+      <p>まずはお気軽にお問い合わせ下さい！</p>
+      <div>
+        <a href="#">LINE@</a>
+        <a href="tel:0948-26-8700">Tel: 0948-26-8700</a>
+        <a href="#">Mail</a>
+      </div>
     </div>
-    <div>
-      
+    <div className="underFooter">
+      <h2>株式会社Lien</h2>
+      <a href="http://www.lien-inc.net/">オフィシャルサイトはこちらから</a>
     </div>
   </footer>
-}
+)
 
 export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Footer from "./footer"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -36,6 +37,7 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
         </div>
+        <Footer />
       </>
     )}
   />
