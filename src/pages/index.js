b750d50e3529ec01ad7794f1dfc813dd7daff606
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-05-19 03:42:26 +0900

    set headline on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,14 @@ import React from "react"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
+import Headline from "../components/headline";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     {/* <Image /> */}
+    <Headline title="INFORMATION" />
+    <Headline title="CONTENTS" />
   </Layout>
 )
 
