d51641f0765982d47f0f258797f13f473d99f1eb
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-05-18 21:22:13 +0900

    change name scss to module.scss

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+
+const Footer = () => {
+  <footer>
+    <div>
+      
+    </div>
+    <div>
+      
+    </div>
+  </footer>
+}
+
+export default Footer
