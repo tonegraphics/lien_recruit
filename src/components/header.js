3140fd04358fafb97cebe237adf0f5e8d8038749
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-19 12:46:54 +0900

    create under bar at current page with nameClasses

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Style from "../styles/components/header.module.scss"
+import classNames from "classnames"
 
-const contents = [
+const baseLink = "http://localhost:8000";
+
+var contents = [
   {
     title: "りあん",
-    link: "/"
+    link: "/",
   },
   {
     title: "未来の保育士さんへ",
-    link: "/message"
+    link: "/message",
   },
   {
     title: "スタッフインタビュー",
-    link: "/interview"
+    link: "/interview",
   },
   {
     title: "見学",
@@ -22,28 +25,48 @@ const contents = [
   },
   {
     title: "募集要項",
-    link: "/recruit"
+    link: "/recruit",
   },
   {
     title: "りあんの魅力",
-    link: "/classrooms"
+    link: "/classrooms",
   },
   {
     title: "制度",
-    link: "/welfare"
-  },
+    link: "/welfare",
+  }
 
 ];
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.url = window.location.href;
+    this.contents = contents;
+   }
+
+  componentWillMount() {
+    this.contents.map((content) => {
+      if(baseLink + content.link === this.url) {
+        content.classes = classNames(
+          Style.listItem,
+          Style.current
+        );
+      } else {
+        content.classes = classNames(
+          Style.listItem
+        );
+      }
+    });
+    console.log(this.contents);
   }
 
   listItems() {
-    const listItems = contents.map((content) =>
-      <div key={content.link} className={Style.listItem}>
-        <a className={Style.link} href={content.link}>{content.title}</a>
+    const listItems = this.contents.map((content) => 
+      <div 
+        key={content.link} 
+        className={content.classes}>
+        <Link to={content.link} className={Style.link}>{content.title}</Link>
       </div>
     );
     return(
