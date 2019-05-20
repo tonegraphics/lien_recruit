545df448e580e202cdc7359725c97b21b4a7b3db
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-20 22:46:07 +0900

    change final color

--- src/components/step.js
+++ src/components/step.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import React from "react"
 import Styles from "../styles/components/step.module.scss"
+import classNames from "classnames"
 
 const stepItems = [
     {
@@ -27,12 +28,27 @@ class Step extends React.Component {
         this.stepItems = stepItems;
     }
 
-    step() {
-        const stepItems = this.stepItems.map((item) =>
-            <div className={Styles.stepItems__stepItem}>
+    stepItem(item) {
+        const itemClass = item.id === 4 ?
+            classNames(
+                Styles.stepItems__stepItem,
+                Styles.yellow
+            ) :
+            classNames(
+                Styles.stepItems__stepItem
+            );
+
+        return (
+            <div className={itemClass}>
                 <p className={Styles.stepItems__stepItem__number}>{item.id}</p>
                 <p className={Styles.stepItems__stepItem__name}>{item.name}</p>
             </div>
+        );
+    }
+
+    step() {
+        const stepItems = this.stepItems.map((item) =>
+            this.stepItem(item)
         );
         return (
             <div className={Styles.stepItems}>{stepItems}</div>
