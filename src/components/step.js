4ac6728848df9508abf24041090810621adc274b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-20 21:02:31 +0900

    add stepItems

--- src/components/step.js
+++ src/components/step.js
@@ -2,16 +2,45 @@ import { Link } from "gatsby"
 import React from "react"
 import Styles from "../styles/components/step.module.scss"
 
+const stepItems = [
+    {
+        id: 1,
+        name: "お問い合わせ"
+    },
+    {
+        id: 2,
+        name: "見学"
+    },
+    {
+        id: 3,
+        name: "面接"
+    },
+    {
+        id: 4,
+        name: "採用"
+    }
+];
+
 class Step extends React.Component {
     constructor(props) {
         super(props);
+        this.stepItems = stepItems;
     }
-    render() {
-        return (
-            <div>
-                <legend>採用!!</legend>
+
+    step() {
+        const stepItems = this.stepItems.map((item) =>
+            <div className={Styles.stepItem}>
+                <p className={Styles.stepItem__number}>{item.id}</p>
+                <p className={Styles.stepItem__name}>{item.name}</p>
             </div>
         );
+        return (
+            <div className={Styles.stepItems}>{stepItems}</div>
+        );
+    }
+
+    render() {
+        return this.step();
     }
 }
 
