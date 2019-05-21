c1fdb9b945312d6a627cba4d0c0e5fb98ee80176
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-21 11:42:36 +0900

    create class classroom

---
+++ src/components/classroom.js
@@ -0,0 +1,19 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import Style from "../styles/components/classroom.module.scss"
+
+class Classroom extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <details />
+            </div>
+        );
+    }
+}
+
+export default Classroom
--- src/pages/classrooms.js
+++ src/pages/classrooms.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Headline from '../components/headline';
-import Step from '../components/step'
+import Classroom from '../components/classroom'
 import Styles from "../styles/pages/classrooms.module.scss"
 import Image from '../components/image';
 
@@ -12,6 +12,7 @@ const ClassroomsPage = () => (
   <Layout>
     <SEO title="Classrooms" />
     <Headline title="教室紹介" />
+    <Classroom />  
   </Layout>
 )
 
